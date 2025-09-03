2ab78071b1f284f66df842d6fb3433c66901a6bd
Shkarupa Alex <shkarupa.alex@gmail.com>
2025-09-03 22:14:34 +0300

    Export meta

--- pumpe/__init__.py
+++ pumpe/__init__.py
@@ -1,7 +1,7 @@
 from pumpe.health import HealthServer
 from pumpe.main import start_pump
-from pumpe.models import BaseModel
+from pumpe.models import BaseModel, PumpMeta
 from pumpe.pumps.base import BasePump
 from pumpe.pumps.model import ModelPump
 
-__all__ = ["BaseModel", "BasePump", "HealthServer", "ModelPump", "start_pump"]
+__all__ = ["BaseModel", "BasePump", "HealthServer", "ModelPump", "PumpMeta", "start_pump"]
